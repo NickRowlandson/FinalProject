07d63581444821f5fcd41dcfb025c21b3876ac2e
NickRowlandson <nicholasrowlandson@gmail.com>
2015-12-13 19:22:52 -0500

    Added the question model

---
+++ server/models/question.js
@@ -0,0 +1,12 @@
+// Import mongoose 
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var QuestionSchema = new Schema({
+  text: String, 
+  options: [{type: Schema.ObjectId, ref: 'Option'}]
+}, {
+  collection: 'questions'
+});
+
+module.exports = mongoose.model('Question', QuestionSchema);
